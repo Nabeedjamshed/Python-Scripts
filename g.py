95c875ba6bdbff77cc7095f665a5e645e3c47d42
Nabeedjamshed <nabeedjamshedali800@gmail.com>
2024-06-22 14:41:36 +0500

    Add new files

---
+++ g.py
@@ -0,0 +1,11 @@
+# minimum = 123456
+# maximum = 0
+# l = list(map(int, input().split()))
+# for i in l:
+#     if i < minimum:
+#         minimum = i
+#         new = i - minimum
+#         maximum = new
+#     else:
+
+# print(maximum)
